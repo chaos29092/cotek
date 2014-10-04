320631fa403ef53199ee2151521cabd9fb24066e
chaos29092 <chaos29092@gmail.com>
2014-10-04 09:04:30 +0800

    jq2.1.1 -> 1.11.1

--- app/views/admin/login.blade.php
+++ app/views/admin/login.blade.php
@@ -58,7 +58,7 @@
 </div>
 
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery.min.js"><\/script>')</script>
 {{ HTML::script('assets/js/plugins.js') }}
 {{ HTML::script('assets/js/main.js') }}
--- app/views/products.blade.php
+++ app/views/products.blade.php
@@ -248,7 +248,7 @@
 </div>
 {{-- 联系表格结束 --}}
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery.min.js"><\/script>')</script>
 {{ HTML::script('assets/js/vendor/bootstrap.min.js') }}
 {{ HTML::script('assets/js/plugins.js') }}
