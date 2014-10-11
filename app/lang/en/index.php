1124483ce4f39a20be7fa84b358197234e6f2c00
chaos29092 <wo94yun>
2014-10-11 09:54:32 +0800

    en,ru,kr,es  ui lang done

--- app/lang/en/index.php
+++ app/lang/en/index.php
@@ -7,6 +7,6 @@ return array(
     "s1"=>"COTEK Router Series",
     "s2"=>"COTEK Laser Series",
     "s3"=>"COTEK Plotter Series",
-    "offer"=>"我们能为您提供什么",
-    "des"=>"刻字机，激光机，切割机等",
+    "offer"=>"What Products Do We Have",
+    "des"=>"Cutting plotter,laser engraving machine and CNC router,etc...",
 );
